f62e943c5748c7bb7805f7761175e1d7831e2448
bruno70i <bruno.npsilva70@gmail.com>
2025-01-22 20:52:32 -0300

    pequenas mudanças no codigo

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+function addaparelho() {
+    var input = document.createElement('input')
+
+    input.style.margin = "10px"
+    input.type = 'number'
+    input.innerHTML = 'hello'
+
+    document.getElementById('new-aparelho').appendChild(input)
+}
+
+
 function calcular() {
     var valor1 = document.getElementById('wattsaparelho01').value
     var valor2 = document.getElementById('wattsaparelho02').value
     var res = document.getElementById('res')
-    var s = valor1 + valor2
-    res.innerHTML = s
+    var s = parseFloat(valor1) + parseFloat(valor2)
+    res.innerHTML = `O resultado da soma de ${valor1} e ${valor2} é ${s}`
+    
 }
-
-/*function addaparelho() {
-    var xxx = documento.getElementById('container');
-    var bloco = document.createElement('input');
-    res.innerHTML = res;
-
-}*/
