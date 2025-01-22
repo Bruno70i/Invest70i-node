e08f8d6d63f4a346c9eb3426bf104fb0a14b3325
bruno70i <bruno.npsilva70@gmail.com>
2025-01-22 20:26:12 -0300

    criando a logica em javascrypt

---
+++ main.js
@@ -0,0 +1,14 @@
+function calcular() {
+    var valor1 = document.getElementById('wattsaparelho01').value
+    var valor2 = document.getElementById('wattsaparelho02').value
+    var res = document.getElementById('res')
+    var s = valor1 + valor2
+    res.innerHTML = s
+}
+
+/*function addaparelho() {
+    var xxx = documento.getElementById('container');
+    var bloco = document.createElement('input');
+    res.innerHTML = res;
+
+}*/
